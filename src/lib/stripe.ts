317a72ea327acded74fa9bb575b3ab0ce527f051
Matt Duff <matt.mpdee@gmail.com>
2025-09-03 16:58:37 +0100

    fix: correct Stripe client-side type to use ReturnType of loadStripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,7 +7,7 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 // Client-side Stripe instance
-let stripePromise: Promise<Stripe | null>;
+let stripePromise: ReturnType<typeof loadStripe>;
 export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
