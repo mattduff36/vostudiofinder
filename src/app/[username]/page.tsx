4846a2fbac73022f2e7e2287d6c6f3d8b2336234
Matt Duff <matt.mpdee@gmail.com>
2025-10-10 23:03:18 +0100

    fix: Add type casting for user prop to resolve enum mismatch

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -270,7 +270,7 @@ export default async function UsernamePage({ params }: UsernamePageProps) {
     return (
       <div className="min-h-screen bg-gray-50 py-8">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-          <EnhancedUserProfile user={user} />
+          <EnhancedUserProfile user={user as any} />
         </div>
       </div>
     );
