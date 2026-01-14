689d6402a0eea164ee5a9f61a820e0177c48dbcf
Matt Duff <matt.mpdee@gmail.com>
2026-01-14 00:16:31 +0000

    Add script to disable exact location toggle for all users

--- disable-exact-location.sql
+++ disable-exact-location.sql
@@ -1,12 +1,12 @@
 -- Disable exact location for all users
--- This sets show_exact_location to '0' for all profiles
-UPDATE profiles
-SET show_exact_location = '0'
-WHERE show_exact_location = '1' OR show_exact_location IS NULL;
+-- This sets show_exact_location to false for all studio_profiles
+UPDATE studio_profiles
+SET show_exact_location = false
+WHERE show_exact_location = true;
 
 -- Verify the update
 SELECT 
   COUNT(*) as total_profiles,
-  SUM(CASE WHEN show_exact_location = '1' THEN 1 ELSE 0 END) as exact_location_on,
-  SUM(CASE WHEN show_exact_location = '0' THEN 1 ELSE 0 END) as exact_location_off
-FROM profiles;
+  SUM(CASE WHEN show_exact_location = true THEN 1 ELSE 0 END) as exact_location_on,
+  SUM(CASE WHEN show_exact_location = false THEN 1 ELSE 0 END) as exact_location_off
+FROM studio_profiles;
